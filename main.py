abad9b764448a14197d3f6ca60d597ce414361be
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2020-08-04 17:40:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys
+import ast
 import yaml
-from time import sleep
+import simplejson
+from time import sleep, time
 from selenium import webdriver
 
 
@@ -12,8 +15,7 @@ class InstaBot:
         chromeOptions = webdriver.ChromeOptions()
         chromeOptions.add_argument("--incognito")
 
-        # Replace 'ChromeDriverPath' with chrome driver's path in your local
-        self.driver = webdriver.Chrome(executable_path='ChromeDriverPath', chrome_options=chromeOptions)
+        self.driver = webdriver.Chrome(executable_path='', chrome_options=chromeOptions)
         self.driver.get("https://instagram.com/")
         sleep(2)
 
@@ -31,9 +33,12 @@ class InstaBot:
         self.driver.find_element_by_xpath(
             "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]"
             ).click()
+
         sleep(3)
         
+    
     def getUnfollowers(self, userId):
+
         try:
             #Navigate to profile and followers
             self.driver.get(f"https://www.instagram.com/{userId}/")
@@ -47,14 +52,15 @@ class InstaBot:
         # Number of followers
         numFollowers = int(self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span").text.replace(",","")) 
 
-        # TO scroll till last in the follower box
+        # To scroll till last in the follower box. If followers count is quite high and 
+        # your net is slow then increase the sleep(N)
         followersBox  = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")
-        for i in range(int(numFollowers/2)): 
+        for i in range(int(numFollowers/5)): 
             self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', followersBox)
             sleep(1)
-            print("Extracting friends %",round((i/(numFollowers/2)*100),2),"from","%100")
+            print("Extracting friends %",round((i/(numFollowers/5)*100),2),"from","%100")
 
-        # Get all the followers
+        # Get the all the followers
         fList  = self.driver.find_elements_by_xpath("//div[@class='PZuss']//li")
         
         # Get the user names of all the followers
@@ -76,17 +82,18 @@ class InstaBot:
         # Number of following
         numFollowing = int(self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[3]/a/span").text.replace(",","")) 
 
-        # TO scroll till last in the following box
+        # To scroll till last in the following box. If followers count is quite high and 
+        # your net is slow then increase the sleep(N)
         followingBox  = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")
-        for i in range(int(numFollowing/2)): 
+        for i in range(int(numFollowing/5)): 
             self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', followingBox)
             sleep(1)
-            print("Extracting friends %",round((i/(numFollowing/2)*100),2),"from","%100")
+            print("Extracting friends %",round((i/(numFollowing/5)*100),2),"from","%100")
 
-        # Get all the following
+        # Get the all the followers
         fList  = self.driver.find_elements_by_xpath("//div[@class='PZuss']//li")
         
-        # Get the user names of all the follwoing accounts
+        # Get the user names of all the followers
         followingList = []
         for ele in fList:
             txt = ele.text
@@ -105,37 +112,39 @@ class InstaBot:
         with open(f"{fileName}.txt", "w") as out:
             out.write(f"Number of accounts who follows you: {len(followersList)}\n")
             out.write(f"Number of accounts you are following: {len(followingList)}\n")
-            out.write(f"Number of accounts who doesn't follow back {len(unFollowingList)}\n")
+            out.write(f"Number of accounts who doesn't follow back: {len(unFollowingList)}\n\n")
             out.write("Following are the accounts who doesnt follow you back:\n")
             out.write(str(unFollowingList))
+
+        # Save as JSON the unfolllowed list to remove later
+        with open(f"{fileName}.json", "w") as out:
+            simplejson.dump({"unfollowed": unFollowingList}, out)
 
 
     def close(self):
         # Close the driver
         self.driver.quit()
 
-        
-# Replace "yamlFilePath.yml" with the path of credentials.yaml file
-credentialsPath = "yamlFilePath.yml"
 
-with open(credentialsPath, "r") as yamlFile:
-    cred = yaml.safe_load(yamlFile)
+if __name__ == "__main__":
+    credentialsPath = sys.argv[1]
+    uIdList = ast.literal_eval(sys.argv[2])
 
-    id = cred["ID"]
-    password = cred["Password"]
+    with open(credentialsPath, "r") as yamlFile:
+        cred = yaml.safe_load(yamlFile)
+        id = cred["ID"]
+        password = cred["Password"]
 
-# Login
-myBot = InstaBot(id, password)
+    # Login
+    myBot = InstaBot(id, password)
 
-# Mention the usernames of accounts you want to check for unfollowers.
-# Note: Followers of the mentioned accounts should be accessible from the 
-# account mentioned in credentials.yml
-uIdList = ["usernames1", "usernames2", "usernames3", "usernames4"]
+    for uid in uIdList:
+        start = time()
+        print(f"This is the current id: {uid}")
+        myBot.getUnfollowers(uid)
+        end = time()
+        print(f"Time taken for {uid}: {round(end-start, 2)} seconds") 
+        print("-----------------------------------------------------------------------\n")
 
-for uid in uIdList:
-    print(f"This is the current id: {uid}")
-    myBot.getUnfollowers(uid)
-    
-# Close
-myBot.close()  
-
+    # Close
+    myBot.close()  
